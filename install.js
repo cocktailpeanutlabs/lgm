3dd6f8650ee13962d11626a24b9e55e0ed9a03b5
cocktailpeanut <cocktailpeanuts@proton.me>
2024-02-08 09:23:49 -0500

    init

---
+++ install.js
@@ -0,0 +1,46 @@
+module.exports = {
+  "cmds": {
+    "win32": {
+      "nvidia": "pip install torch torchvision torchaudio xformers --index-url https://download.pytorch.org/whl/cu121",
+      "amd": "pip install torch-directml",
+      "cpu": "pip install torch torchvision torchaudio"
+    },
+    "darwin": "pip install torch torchvision torchaudio",
+    "linux": {
+      "nvidia": "pip install torch torchvision torchaudio xformers --index-url https://download.pytorch.org/whl/cu121",
+      "amd": "pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.7",
+      "cpu": "pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu"
+    }
+  },
+  "requires": [{
+    "type": "conda",
+    "name": "ffmpeg",
+    "args": "-c conda-forge"
+  }],
+  "run": [{
+    "method": "shell.run",
+    "params": {
+      "message": "git clone https://huggingface.co/spaces/cocktailpeanut/LGM app"
+    }
+  }, {
+    "method": "shell.run",
+    "params": {
+      "venv": "env",
+      "path": "app",
+      "message": [
+        "{{(platform === 'darwin' ? self.cmds.darwin : (['nvidia', 'amd'].includes(gpu) ? self.cmds[platform][gpu] : self.cmds[platform].cpu))}}",
+        "pip install -r requirements.txt",
+      ]
+    }
+  }, {
+    "method": "fs.share",
+    "params": {
+      "venv": "app/env"
+    }
+  }, {
+    "method": "notify",
+    "params": {
+      "html": "Click the 'start' tab to get started!"
+    }
+  }]
+}
