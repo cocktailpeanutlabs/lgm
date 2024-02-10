580805aa5483abcbb55bc1ebfc8adee38ce1efd6
cocktailpeanut <cocktailpeanuts@proton.me>
2024-02-10 02:51:09 -0500

    update

--- start.js
+++ start.js
@@ -1,27 +1,32 @@
-module.exports = {
-  daemon: true,
-  run: [{
-    "method": "shell.run",
-    "params": {
-      "path": "app",
-      "conda": "env",
-      "message": [
-        "{{platform === 'win32' ? 'set' : 'env'}}",
-        "nvcc --version",
-        "python app.py",
-      ],
-      "on": [{ "event": "/http:\/\/[0-9.:]+/", "done": true }]
-    }
-  }, {
-    "method": "local.set",
-    "params": {
-      "url": "{{input.event[0]}}"
-    }
-  }, {
-    "method": "proxy.start",
-    "params": {
-      "uri": "{{local.url}}",
-      "name": "Local Sharing"
-    }
-  }]
+module.exports = async (kernel) => {
+  return {
+    daemon: true,
+    run: [{
+      "method": "shell.run",
+      "params": {
+        "path": "app",
+        "conda": "env",
+        "env": {
+          "CUDA_HOME": "{{path.resolve(cwd, 'env')}}"
+        },
+        "message": [
+          "{{platform === 'win32' ? 'set' : 'env'}}",
+          "nvcc --version",
+          "python app.py",
+        ],
+        "on": [{ "event": "/http:\/\/[0-9.:]+/", "done": true }]
+      }
+    }, {
+      "method": "local.set",
+      "params": {
+        "url": "{{input.event[0]}}"
+      }
+    }, {
+      "method": "proxy.start",
+      "params": {
+        "uri": "{{local.url}}",
+        "name": "Local Sharing"
+      }
+    }]
+  }
 }
