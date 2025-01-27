5b8f7d16d86f8baaa66561f0686efa7af576c885
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2025-01-27 22:32:00 +0100

    switch to uv

--- install.js
+++ install.js
@@ -1,22 +1,17 @@
 module.exports = {
   "cmds": {
     "win32": {
-      "nvidia": "pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 xformers --index-url https://download.pytorch.org/whl/cu121",
-      "amd": "pip install torch-directml torchvision torchaudio",
-      "cpu": "pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1"
+      "nvidia": "uv pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 xformers --index-url https://download.pytorch.org/whl/cu121",
+      "amd": "uv pip install torch-directml torchvision torchaudio numpy==1.26.4",
+      "cpu": "uv pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cpu"
     },
-    "darwin": "pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1",
+    "darwin": "uv pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cpu",
     "linux": {
-      "nvidia": "pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 xformers --index-url https://download.pytorch.org/whl/cu121",
-      "amd": "pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/rocm5.7",
-      "cpu": "pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cpu"
+      "nvidia": "uv pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 xformers --index-url https://download.pytorch.org/whl/cu121",
+      "amd": "uv pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/rocm5.7",
+      "cpu": "uv pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cpu"
     }
   },
-  "requires": [{
-    "type": "conda",
-    "name": "ffmpeg",
-    "args": "-c conda-forge"
-  }],
   "run": [{
     "method": "shell.run",
     "params": {
@@ -31,9 +26,9 @@ module.exports = {
         "{{platform === 'win32' ? 'where pip' : 'which pip'}}",
         "{{(platform === 'darwin' ? self.cmds.darwin : (['nvidia', 'amd'].includes(gpu) ? self.cmds[platform][gpu] : self.cmds[platform].cpu))}}",
         "{{platform === 'win32' ? 'where pip' : 'which pip'}}",
-        "pip install opencv-python",
+        "uv pip install opencv-python",
         "{{platform === 'win32' ? 'where pip' : 'which pip'}}",
-        "pip install -r requirements.txt",
+        "uv pip install -r requirements.txt",
         "{{platform === 'win32' ? 'where pip' : 'which pip'}}",
         "conda install -y cudnn libzlib-wapi -c conda-forge",
         "conda install -y cuda -c nvidia/label/cuda-12.1.0"
